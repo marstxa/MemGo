418aab1c16595aff4a320c86e06454f7efab7b35
marstxa <marstxa@gmail.com>
2026-09-04 23:11:20 +0100

    feat: implement RESP parser and serializer with PING support

--- main.go
+++ main.go
@@ -2,44 +2,41 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
+
+	"github.com/marstxa/resp"
 )
 
 func main() {
-    fmt.Println("Listening on port :6379")
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Listening on port :6379...")
 
-    // Create a new server
-    l, err := net.Listen("tcp", ":6379")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
 
-    // Listen for connections
-    conn, err := l.Accept()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	for {
+		// initialise parser by wrapping the connection
+		parser := resp.NewResp(conn)
+		
+		// attempt to parse a full RESP value
+		value, err := parser.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-    defer conn.Close()
+		// print the parsed command array to the server logs
+		fmt.Printf("Parsed Command: %+v\n", value.Array)
 
-    for {
-        buffer := make([]byte, 1024)
-
-        // read message from client
-        _, err = conn.Read(buffer)
-        if err != nil {
-            if err == io.EOF {
-                break
-            }
-            fmt.Println("error reading from client: ", err.Error())
-            os.Exit(1)
-        }
-
-        // ignore
-        conn.Write([]byte("+OK\r\n"))
-    }
+		// ignore request and send back PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
---
+++ resp/resp.go
@@ -0,0 +1,127 @@
+package resp
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Value struct {
+	Typ string
+	Str string
+	Num int
+	Bulk string
+	Array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+
+}
+
+// NewResp  creates a new wrapper aroundd the TCP connection
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error){
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+
+	return line[:len(line) -2], n, nil
+}
+
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err 
+	}
+
+	i64, err := strconv.ParseInt(string(line), 10, 64)
+	if err != nil {
+		return 0, n, err
+	}
+	return int(i64), n, nil
+}
+
+func (r *Resp) Read() (Value, error) {
+	_type, err := r.reader.ReadByte()
+
+	if err != nil {
+		return Value{}, err
+	}
+
+	switch _type {
+	case ARRAY:
+		return r.readArray()
+	case BULK:
+		return r.readBulk()
+	default:
+		fmt.Printf("Unknown type: %v", string(_type))
+		return Value{}, nil
+	}
+}
+
+func (r *Resp) readArray() (Value, error){
+	v := Value{}
+	v.Typ = "array"
+
+	// read len of the array
+	length, _, err := r.readInteger()
+	if err != nil {
+		return v, err 
+	}
+
+	// for each line parse and read the value
+	v.Array = make([]Value, length)
+	for i:=0;i < length; i++ {
+		val, err := r.Read()
+		if err != nil {
+			return v, err 
+		}
+
+		// add parsed value to array
+		v.Array[i] = val
+	}
+
+	return v, nil
+}
+
+func (r *Resp) readBulk() (Value, error){
+	v := Value{}
+	v.Typ = "bulk"
+
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err 
+	}
+
+	bulk := make([]byte, len)
+	r.reader.Read(bulk)
+	v.Bulk = string(bulk)
+
+	// Read the trailing \r\n
+	r.readLine()
+
+	return v, nil
+}
+ 
