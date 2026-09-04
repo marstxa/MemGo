2fdb1d97a5a2217707aa07bff2c92255b82e5dfd
marstxa <marstxa@gmail.com>
2026-09-04 20:58:15 +0100

    feat: setup TCP listener on port 6379

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
 
 func main() {
-    fmt.Println("Starting my custom Redis server...")
+    fmt.Println("Listening on port :6379")
+
+    // Create a new server
+    l, err := net.Listen("tcp", ":6379")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    // Listen for connections
+    conn, err := l.Accept()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    defer conn.Close()
+
+    for {
+        buffer := make([]byte, 1024)
+
+        // read message from client
+        _, err = conn.Read(buffer)
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            fmt.Println("error reading from client: ", err.Error())
+            os.Exit(1)
+        }
+
+        // ignore
+        conn.Write([]byte("+OK\r\n"))
+    }
 }
