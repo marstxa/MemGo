c3a1b5002e28e0fe3b5029e19a5ed936a701bdf7
marstxa <marstxa@gmail.com>
2026-09-06 17:56:13 +0100

    feat: implement data persistance using aof including methods like read and write to the file "database.aof"

---
+++ aof/aof.go
@@ -0,0 +1,61 @@
+package aof
+
+import (
+	"bufio"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/marstxa/resp"
+)
+
+type Aof struct {
+	File *os.File
+	Rd *bufio.Reader
+	Mu sync.Mutex
+}
+
+func NewAof(path string) (*Aof, error) {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+
+	if err != nil {
+		return nil, err 
+	}
+
+	aof := &Aof{
+		File: file,
+		Rd: bufio.NewReader(file),
+	}
+
+	// start goroutine to sync AOF to disk every 1 second
+	go func ()  {
+		for {
+			aof.Mu.Lock()
+			aof.File.Sync()
+			aof.Mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return aof, nil
+}
+
+func (aof *Aof) Close() error {
+	aof.Mu.Lock()
+	defer aof.Mu.Unlock()
+
+	return aof.File.Close()
+}
+
+
+func (aof *Aof) Write(value resp.Value) error {
+	aof.Mu.Lock()
+	defer aof.Mu.Unlock()
+
+	_, err := aof.File.Write(value.Marshal())
+	if err != nil {
+		return err 
+	}
+
+	return nil
+} 
--- main.go
+++ main.go
@@ -5,21 +5,47 @@ import (
 	"net"
 	"strings"
 
+	"github.com/marstxa/aof"
 	"github.com/marstxa/handler"
 	"github.com/marstxa/resp"
 )
 
 func main() {
+	fmt.Println("Listening on port :6379")
+
+	// create new server
 	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		return
+	}
+
+	// data persistence with aof
+	aof, err := aof.NewAof("database.aof")
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("Listening on port :6379...")
+	defer aof.Close()
 
+	aof.Read(func(value resp.Value) {
+		command := strings.ToUpper(value.Array[0].Bulk)
+		args := value.Array[1:]
+
+		handler, ok := handler.Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
+
+	// listen for connections
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error accepting connection: ", err.Error())
 		return
 	}
 	defer conn.Close()
@@ -56,6 +82,10 @@ func main() {
 			fmt.Println("Invalid command ", command)
 			writer.Write(resp.Value{Typ: "string", Str: ""})
 			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
 		}
 
 		result := handler(args)
